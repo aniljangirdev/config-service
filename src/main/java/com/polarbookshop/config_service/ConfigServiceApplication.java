0c9c7e9d81e9a56c6f68ccfe93c3a0ec8367f6d2
Anil Jangir <aniljangir212@gmail.com>
2024-05-29 15:13:56 +0530

    added spring cofing server

--- src/main/java/com/polarbookshop/config_service/ConfigServiceApplication.java
+++ src/main/java/com/polarbookshop/config_service/ConfigServiceApplication.java
@@ -3,6 +3,7 @@ package com.polarbookshop.config_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableConfigServer
 @SpringBootApplication
